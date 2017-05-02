5cb22c382f50ed1f109d5731a97920a8c892f0fd
totovivi <vicente.thms@gmail.com>
2017-05-02 15:26:52 +0100

    own network

--- tensorflow_tests/mnist.py
+++ tensorflow_tests/mnist.py
@@ -1,31 +1,12 @@
+import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
+
 mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 
-import tensorflow as tf
 sess = tf.InteractiveSession()
 
 x = tf.placeholder(tf.float32, shape=[None, 784])
 y_ = tf.placeholder(tf.float32, shape=[None, 10])
-
-W = tf.Variable(tf.zeros([784, 10]))
-b = tf.Variable(tf.zeros([10]))
-
-sess.run(tf.global_variables_initializer())
-
-y = tf.matmul(x, W) + b
-
-cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=y, labels=y_))
-
-train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
-
-file_writer = tf.summary.FileWriter('./summaries', sess.graph)
-for i in range(1000):
-	batch = mnist.train.next_batch(100)
-	train_step.run(feed_dict={x: batch[0], y_: batch[1]})
-
-correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-print 'Softmax accuracy:', accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels})
 
 def weight_variable(shape):
 	initial = tf.truncated_normal(shape, stddev=0.1)
@@ -66,14 +47,38 @@ b_fc2 = bias_variable([10])
 y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
 
 cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=y_conv, labels=y_))
-train_step = tf.train.AdamOptimizer(1e-3).minimize(cross_entropy)
+train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 sess.run(tf.global_variables_initializer())
-for i in range(10000):
+for i in range(5000):
 	batch = mnist.train.next_batch(50)
-	if i%1000==0:
+	if i%1==0:
 		train_accuracy = accuracy.eval(feed_dict={x:batch[0], y_:batch[1], keep_prob: 1.0})
-		print 'step %d, accuracy %g'%(i, train_accuracy)
+		print '|' * int(train_accuracy * 50), int(train_accuracy * 100)
 	train_step.run(feed_dict={x:batch[0], y_:batch[1], keep_prob: 0.5})
 
 print 'test accuracy %g'%accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0})
+
+
+
+# W = tf.Variable(tf.zeros([784, 10]))
+
+# b = tf.Variable(tf.zeros([10]))
+
+# sess.run(tf.global_variables_initializer())
+
+# y = tf.matmul(x, W) + b
+
+# cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=y, labels=y_))
+
+# train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+
+# file_writer = tf.summary.FileWriter('./summaries', sess.graph)
+# for i in range(1000)
+# 	batch = mnist.train.next_batch(100)
+# 	train_step.run(feed_dict={x: batch[0], y_: batch[1]})
+
+# correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+# accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+# print 'Softmax accuracy:', accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels})
