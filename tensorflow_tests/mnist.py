1704a99d1b6cc56e09c6241cf4b4f1cb52b475de
totovivi <vicente.thms@gmail.com>
2017-07-06 12:08:15 +0200

    from pycharm

--- tensorflow_tests/mnist.py
+++ tensorflow_tests/mnist.py
@@ -1,6 +1,8 @@
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 
+print 'useless'
+
 mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 
 sess = tf.InteractiveSession()
